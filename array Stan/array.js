1942aa389da01d5554ff328ae2cb0e148e2d0a90
Mariana <arianatj@i.ua>
2022-09-26 00:09:15 +0300

    bubble sorting for array

---
+++ array Stan/array.js
@@ -0,0 +1,16 @@
+//перебір масиву
+//bubble sorting
+// const arr = [10, 2, 4, 67, 97, 139, 3, 7]
+//
+// for ( let i = 0; i<arr.length-1; i++) {
+//     for (let j=i+1; j<arr.length; j++ ){
+//         if(arr[i]>arr[j]){   // > сортування від найменшого до найбільшого, < від найбільшого до найменшого
+//             let temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j]=temp;
+//         }
+//     }
+// }
+// console.log(arr)
+//console.log(arr[arr.length-1]); //arr.at(-1) //max число в масиві, після перебору
+//console.log(arr[arr.length-2]); //arr.at(-2) // друге мах число в масиві
